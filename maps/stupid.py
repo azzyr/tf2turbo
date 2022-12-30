e0c1760028119e37dfe70f0b3790b59721b42f6f
azzy <azzyr@protonmail.com>
2022-12-30 17:43:01 +0200

    oops

--- maps/stupid.py
+++ maps/stupid.py
@@ -1,6 +1,13 @@
 import shutil
 import os
 
+directory = '.'
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f) and filename.endswith('.bsp.bz2'):
+        shutil.copyfile('master_items_game.txt', filename[:len(filename) - 8] + '_items_game.txt')
+        shutil.copyfile('master_level_sounds.txt', filename[:len(filename) - 8] + '_level_sounds.txt')
+
 directory = './othermaps'
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
